c75fd8b3f81011c4fae99fc9a837610c1620fef1
Tata <tata4348@mail.ru>
2019-07-01 20:47:01 +0300

    deb API

--- app/directory/bot.php
+++ app/directory/bot.php
@@ -9,6 +9,13 @@
     $chat_id = $result["message"]["chat"]["id"]; //Уникальный идентификатор пользователя 1
     $name = $result["message"]["from"]["username"]; //Юзернейм пользователя
     $keyboard = [["Последние статьи"],["Картинка"],["Гифка"],["View Hello, world!"]]; //Клавиатура
+    // 1. инициализация
+    $ch = curl_init();
+
+    // 2. указываем параметры, включая url
+    curl_setopt($ch, CURLOPT_URL, "https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=https://developers.google.com");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
 
     if($text){
          if ($text == "/start") {
@@ -40,12 +47,21 @@
         }elseif ($text == "Гифка") {
             $url = "https://68.media.tumblr.com/bd08f2aa85a6eb8b7a9f4b07c0807d71/tumblr_ofrc94sG1e1sjmm5ao1_400.gif";
             $telegram->sendDocument([ 'chat_id' => $chat_id, 'document' => $url, 'caption' => "Описание." ]);
-        }elseif ($text == "Последние статьи") {
-            $html=simplexml_load_file('http://netology.ru/blog/rss.xml');
-            foreach ($html->channel->item as $item) {
-	     $reply .= "\xE2\x9E\xA1 ".$item->title." (<a href='".$item->link."'>читать</a>)\n";
-        	}
-            $telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode' => 'HTML', 'disable_web_page_preview' => true, 'text' => $reply ]);
+        }elseif ($text == "Запрос API") {
+              $output = curl_exec($ch);
+              // А вдруг ошибочка?
+              if ($output === FALSE) {
+                //Тут-то мы о ней и скажем
+                  echo "cURL Error: " . curl_error($ch);
+                return;
+              }
+              
+           //Получаем информацию о запросе
+              $info = curl_getinfo($ch);
+              //Выводим какую-то инфомрацию
+              echo 'Запрос выполнился за  ' . $info['total_time'] . ' сек. к URL: ' . $info['url'];
+              
+              }
         }else{
         	$reply = "По запросу \"<b>".$text."</b>\" ничего не найдено.";
         	$telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode'=> 'HTML', 'text' => $reply ]);
