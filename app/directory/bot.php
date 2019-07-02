312220460f4395caae97a47fe8df718971b1428e
Tata <tata4348@mail.ru>
2019-07-02 20:07:09 +0300

    deb 02-07-12

--- app/directory/bot.php
+++ app/directory/bot.php
@@ -71,12 +71,11 @@
             $reply = "Информация с помощью.";
             $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply ]);           
         }elseif ($separatedText[0] == "/check") {
-            $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $separatedText[0] ]);
             array_shift($separatedText);
-            $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $separatedText[0] ]);
             foreach ($separatedText as $currentUrl)
             {
               $urlForPingApi = "https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=".$currentUrl."&key=AIzaSyDZk6qaWml22Q8CiYms9Y8u4IkZ2rIsRVs";
+              $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $urlForPingApi ]);
               $responseJson = getResponseApi($urlForPingApi);
               $reply = getFormatedJson($responseJson);
               $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply ]);
