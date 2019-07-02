4bf315f4e55ee31738a9a2145489abf39353aafc
Tata <tata4348@mail.ru>
2019-07-02 21:21:23 +0300

    delete /app

--- directory/bot.php
+++ directory/bot.php
@@ -1,35 +1,89 @@
 <?php
     include('../vendor/autoload.php'); //Подключаем библиотеку
     use Telegram\Bot\Api; 
-
     $telegram = new Api('831949384:AAEdN3KQz00sMaFto2yLotRGETTFmw_dk7c'); //Устанавливаем токен, полученный у BotFather
     $result = $telegram -> getWebhookUpdates(); //Передаем в переменную $result полную информацию о сообщении пользователя
     
     $text = $result["message"]["text"]; //Текст сообщения
+    $separatedText = explode(" ", $text);
     $chat_id = $result["message"]["chat"]["id"]; //Уникальный идентификатор пользователя 1
     $name = $result["message"]["from"]["username"]; //Юзернейм пользователя
-    $keyboard = [["Последние статьи"],["Картинка"],["Гифка"]]; //Клавиатура
+    $keyboard = [["Список команд"],["Ping API"],["Получить ответ от API"]]; //Клавиатура
 
+    
+    function getFormatedJson($responseJson):string
+    {
+        $textJson = json_encode($responseJson, JSON_PRETTY_PRINT);
+            
+        return $textJson;
+    }
+    
+    function getResponseApi($urlApi, $keyParametr): string
+    {return $urlApi." ".$keyParametr;
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $urlApi);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+      
+        $output = curl_exec($ch);
+        if ($output === FALSE) 
+        {
+            $textJson = "cURL Error: ".curl_error($ch);
+        }
+        
+        $textJson = getFormatedJson($output);
+         
+        $info = curl_getinfo($ch);
+        if ($info === FALSE) 
+        {
+            $responseInfo = "cURL Error: ".curl_error($ch);
+        }
+        else
+        {
+            $responseInfo = "Запрос выполнился за  ".$info['total_time'].' сек. к URL: '.$info['url'];
+        }
+      
+        curl_close($ch);
+        
+        if ($keyParametr === "info")
+        {
+            return $responseInfo;
+        }elseif ($keyParametr === "json")
+        {
+            return $textJson;
+        }
+        
+    }
+    
     if($text){
-         if ($text == "/start") {
-            $reply = "Добро пожаловать в бота!";
-            $reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => false ]);
-            $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup ]);
-        }elseif ($text == "/help") {
+        if ($text == "/start") {
+          if (isset($name))
+          {
+              $reply = "Добро пожаловать в бота, ".$name."!";
+              $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply ]); 
+          }else 
+          {
+              $reply = "Добро пожаловать в бота, незнакомец!";
+              $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply ]);
+          }
+        }elseif ($text == "Список команд") {
             $reply = "Информация с помощью.";
+            $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply ]);           
+        }elseif ($separatedText[0] == "/check") {
+            array_shift($separatedText);
+            foreach ($separatedText as $currentUrl)
+            {
+                if(substr($currentUrl, 8) === "https://")  
+                {
+                    $urlForPingApi = "https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=".$currentUrl."&key=AIzaSyDZk6qaWml22Q8CiYms9Y8u4IkZ2rIsRVs";
+                    $reply = getResponseApi($urlForPingApi, "json");
+                    $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply ]);
+                }
+            }
+        }elseif ($text == "Ping API") {
+            $urlForPingApi = "https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=https://developers.google.com&key=AIzaSyDZk6qaWml22Q8CiYms9Y8u4IkZ2rIsRVs";
+            $reply = getResponseApi($urlForPingApi, "info");
             $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply ]);
-        }elseif ($text == "Картинка") {
-            $url = "https://68.media.tumblr.com/6d830b4f2c455f9cb6cd4ebe5011d2b8/tumblr_oj49kevkUz1v4bb1no1_500.jpg";
-            $telegram->sendPhoto([ 'chat_id' => $chat_id, 'photo' => $url, 'caption' => "Описание." ]);
-        }elseif ($text == "Гифка") {
-            $url = "https://68.media.tumblr.com/bd08f2aa85a6eb8b7a9f4b07c0807d71/tumblr_ofrc94sG1e1sjmm5ao1_400.gif";
-            $telegram->sendDocument([ 'chat_id' => $chat_id, 'document' => $url, 'caption' => "Описание." ]);
-        }elseif ($text == "Последние статьи") {
-            $html=simplexml_load_file('http://netology.ru/blog/rss.xml');
-            foreach ($html->channel->item as $item) {
-	     $reply .= "\xE2\x9E\xA1 ".$item->title." (<a href='".$item->link."'>читать</a>)\n";
-        	}
-            $telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode' => 'HTML', 'disable_web_page_preview' => true, 'text' => $reply ]);
         }else{
         	$reply = "По запросу \"<b>".$text."</b>\" ничего не найдено.";
         	$telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode'=> 'HTML', 'text' => $reply ]);
