142abb6a55f2ccbf69c251882a3b203b3baaf528
Tata <tata4348@mail.ru>
2019-07-04 01:55:42 +0300

    47

--- db_handler.php
+++ db_handler.php
@@ -4,13 +4,20 @@
     const DB_USERNAME = "b60754546ea096";
     const DB_PASSWORD = "36da8d02";
     const DB_NAME = "heroku_7fe864cef8db15a";
+    const SQL_INSERT = "insert into {table_name} {column_name} values({values})";
 
     class dbColumnName {
-        const CHAT_ID = "chat_id";
-        const USERNAME = "username";
-        const CREATE_DATA = "create_data";
-        const USER_LAST_CONNECTION_DATA = "user_last_connect_data";
-        const USER_URL = "user_url";
+        const CHAT_ID = 'chat_id';
+        const USERNAME = 'username';
+        const CREATE_DATA = 'create_data';
+        const USER_LAST_CONNECTION_DATA = 'user_last_connect_data';
+        const USER_URL = 'user_url';
+    }
+
+    class dbTableName {
+        const USER = 'user';
+        const USER_LAST_CONNECT = 'user_last_connect';
+        const USER_URL = 'user_url';
     }
 
     global $db;
@@ -18,19 +25,21 @@
 
     mysqli_select_db($db, DB_NAME) or die();
 
-    mysqli_query("SET NAMES utf8", $db);
-    mysqli_query("SET CHARACTER SET utf8", $db);
-    mysqli_query("SET COLLATION_CONNECTION='utf8_general_ci'", $db); 
+    mysqli_query($db, "SET NAMES utf8");
+    mysqli_query($db, "SET CHARACTER SET utf8");
+    mysqli_query($db, "SET COLLATION_CONNECTION='utf8_general_ci'"); 
     setlocale(LC_ALL,"ru_RU.UTF8");
 
-
-
-
-/*function createNewUser()
-{
-    $data = Array(CHAT_ID  =>  " admin ", USERNAME => " John ", CREATE_DATA => '01.01.2019'); 
-    $id = $db -> insert('user', $ data);
-}*/
+    function create_user($name, $chat_id)
+    {
+        global $db;
+        $name = mysqli_real_escape_string($name);
+        $chat_id = mysqli_real_escape_string($chat_id);
+        $query_replase_table = str_replace("{table_name}", USER, SQL_INSERT);
+        $query_replase_column = str_replace("{column_name}", CHAT_ID . ', ' . USERNAME . ', ' . CREATE_DATA, $query_replase_table);
+        $query = str_replace("{values}", "'" . $chat_id . "', '" . $name . "', '" . date("m.d.y") . "'", $query_replase_column);
+        mysqli_query($db, $query) or die();
+    }
 ?>
 
 
