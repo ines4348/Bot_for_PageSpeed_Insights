3347da96a51f68b90957cb91c56e8d80814c5e0e
Tata <tata4348@mail.ru>
2019-07-04 02:28:58 +0300

    59

--- db_handler.php
+++ db_handler.php
@@ -40,10 +40,10 @@
         $chat_id = mysqli_real_escape_string($db, $chat_id);
         $query_replase_table = str_replace("{table_name}", dbTableName::USER, SQL_INSERT);
         $query_replase_column = str_replace("{column_name}", dbColumnName::CHAT_ID . ', ' . dbColumnName::USERNAME . ', ' . dbColumnName::CREATE_DATA, $query_replase_table);
-        $query = str_replace("{values}", "'" . $chat_id . "', '" . $name . "', '" . date("m.d.y") . "'", $query_replase_column);return $query;
+        $query = str_replace("{values}", "'" . $chat_id . "', '" . $name . "', '" . date("m.d.y") . "'", $query_replase_column);
         mysqli_query($db, $query) or die();
         mysqli_close($db);
-        
+        return "Пользователь добавлен";
     }
 
     
