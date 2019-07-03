2cc8dd7cb08c1936aed327ae7498bdc168da5b1b
Tata <tata4348@mail.ru>
2019-07-04 01:15:16 +0300

    41

--- db_handler.php
+++ db_handler.php
@@ -21,8 +21,8 @@
     mysql_select_db(DB_NAME, $db) or die();
 
     mysql_query("SET NAMES utf8", $db);
-    mysql_query("SET CHARACTER SET utf8", $db );
-    mysql_query("SET COLLATION_CONNECTION="utf8_general_ci"", $db ); 
+    mysql_query("SET CHARACTER SET utf8", $db);
+    mysql_query("SET COLLATION_CONNECTION='utf8_general_ci'", $db); 
     setlocale(LC_ALL,"ru_RU.UTF8");
 
 
