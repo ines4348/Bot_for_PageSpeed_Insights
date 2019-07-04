4a1e14bf400dc7ac986230825aeef1ac78af04a7
Tata <tata4348@mail.ru>
2019-07-04 04:28:28 +0300

    93

--- db_handler.php
+++ db_handler.php
@@ -4,7 +4,7 @@
     const DB_USERNAME = "b60754546ea096";
     const DB_PASSWORD = "36da8d02";
     const DB_NAME = "heroku_7fe864cef8db15a";
-    const SQL_INSERT = "insert into {table_name} ({column_name}) values ('{values}')";
+    const SQL_INSERT = "insert into {table_name} ({column_name}) values ('{values}');";
     const DATE_FORMAT = "y.m.d";
     const SEPARATOR = ", ";
     const SEPARATOR_VALUE = "', '";
@@ -60,9 +60,7 @@
         $query_replase_table = str_replace("{table_name}", dbTableName::USER, SQL_INSERT);
         $query_replase_column = str_replace("{column_name}", dbColumnName::CHAT_ID . SEPARATOR . dbColumnName::USERNAME . SEPARATOR . dbColumnName::CREATE_DATA, $query_replase_table);
         $query = str_replace("{values}", $chat_id . SEPARATOR_VALUE . $name . SEPARATOR_VALUE . date(DATE_FORMAT), $query_replase_column);
-        return $query;
         mysqli_query($db, $query) or die();
-        
         mysqli_close($db);
         return "Пользователь добавлен";
     }
